c90c4c52274c693213967af0df9a3d0670c882f8
emmmm <3108775859@qq.com>
2020-05-06 16:48:20 +0800

    使用spring session + redis

--- src/main/java/hello/configuration/SessionConfig.java
+++ src/main/java/hello/configuration/SessionConfig.java
@@ -2,9 +2,10 @@ package hello.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
 @Configuration
 @EnableRedisHttpSession
-public class SessionConfig {
+public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
 }
 
