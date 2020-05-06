a1073ac7397c324b7fcfc1e7cc563d0442571e96
emmmm <3108775859@qq.com>
2020-05-06 16:52:13 +0800

    update somethings

--- src/main/java/hello/configuration/RedisConfig.java
+++ src/main/java/hello/configuration/RedisConfig.java
@@ -1,17 +1,20 @@
 package hello.configuration;
 
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
-//    @Bean
-//    RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setConnectionFactory(factory);
-//        return redisTemplate;
-//    }
+    @Bean
+    RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+        return redisTemplate;
+    }
 //    @Bean
 //    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
 //        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
