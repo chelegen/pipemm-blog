2e43228dfb82315d8a15ff875150900d97ceed33
emmmm <3108775859@qq.com>
2020-05-06 17:59:17 +0800

    删除了无用代码，并在TravisCI中配置Redis

--- src/main/java/hello/configuration/RedisConfig.java
+++ src/main/java/hello/configuration/RedisConfig.java
@@ -15,15 +15,4 @@ public class RedisConfig {
         redisTemplate.setConnectionFactory(factory);
         return redisTemplate;
     }
-//    @Bean
-//    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-//        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setConnectionFactory(redisConnectionFactory);
-//        // 设置value的序列化规则和 key的序列化规则
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-//        redisTemplate.afterPropertiesSet();
-//        return redisTemplate;
-//    }
-
 }
